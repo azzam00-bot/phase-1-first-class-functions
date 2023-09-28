b01efca4fa4b7c0708a8b5e01f7df97ca312ccb7
Azzam ALmasslamany <azzammeri24@gmail.com>
2023-09-28 16:01:00 +0300

    I Write The Full Code Here

--- index.js
+++ index.js
@@ -1,36 +1,99 @@
-// index.js
+// // index.js
 
-// The receivesAFunction function
-function receivesAFunction(callback) {
-  callback();
-}
+// // The receivesAFunction function
+// function receivesAFunction(callback) {
+//   callback();
+// }
 
-// The returnsANamedFunction function
-function returnsANamedFunction() {
-  function namedFunction() {
-    console.log("This is a named function");
-  }
-  return namedFunction;
-}
+// // The returnsANamedFunction function
+// function returnsANamedFunction() {
+//   function namedFunction() {
+//     console.log("This is a named function");
+//   }
+//   return namedFunction;
+// }
 
-// The returnsAnAnonymousFunction function
-function returnsAnAnonymousFunction() {
-  return function() {
-    console.log("This is an anonymous function");
+// // The returnsAnAnonymousFunction function
+// function returnsAnAnonymousFunction() {
+//   return function() {
+//     console.log("This is an anonymous function");
+//   };
+// }
+
+// // Example usage of the functions
+
+// // Using receivesAFunction
+// receivesAFunction(function() {
+//   console.log("Callback function is called");
+// });
+
+// // Using returnsANamedFunction
+// const namedFunc = returnsANamedFunction();
+// namedFunc();
+
+// // Using returnsAnAnonymousFunction
+// const anonymousFunc = returnsAnAnonymousFunction();
+// anonymousFunc();
+
+// app.js
+
+// returnFirstTwoDrivers
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2);
+};
+
+// returnLastTwoDrivers
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+};
+
+// selectingDrivers
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// createFareMultiplier
+const createFareMultiplier = function(integer) {
+  return function(fare) {
+    return fare * integer;
   };
-}
+};
 
-// Example usage of the functions
+// fareDoubler
+const fareDoubler = createFareMultiplier(2);
 
-// Using receivesAFunction
-receivesAFunction(function() {
-  console.log("Callback function is called");
-});
+// fareTripler
+const fareTripler = createFareMultiplier(3);
 
-// Using returnsANamedFunction
-const namedFunc = returnsANamedFunction();
-namedFunc();
+// selectDifferentDrivers
+const selectDifferentDrivers = function(drivers, returnDrivers) {
+  return returnDrivers(drivers);
+};
 
-// Using returnsAnAnonymousFunction
-const anonymousFunc = returnsAnAnonymousFunction();
-anonymousFunc();
+// Call returnFirstTwoDrivers
+const drivers = ['John', 'Jane', 'Mike', 'Sarah'];
+const firstTwoDrivers = returnFirstTwoDrivers(drivers);
+console.log(firstTwoDrivers); // Output: ['John', 'Jane']
+
+// Call returnLastTwoDrivers
+const lastTwoDrivers = returnLastTwoDrivers(drivers);
+console.log(lastTwoDrivers); // Output: ['Mike', 'Sarah']
+
+// Access selectingDrivers array
+console.log(selectingDrivers); // Output: [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+// Call createFareMultiplier
+const fareMultiplier = createFareMultiplier(2);
+const fare = 10;
+const multipliedFare = fareMultiplier(fare);
+console.log(multipliedFare); // Output: 20
+
+// Call fareDoubler
+const doubledFare = fareDoubler(fare);
+console.log(doubledFare); // Output: 20
+
+// Call fareTripler
+const tripledFare = fareTripler(fare);
+console.log(tripledFare); // Output: 30
+
+// Call selectDifferentDrivers
+const selectedDrivers = selectDifferentDrivers(drivers, returnFirstTwoDrivers);
+console.log(selectedDrivers); // Output: ['John', 'Jane']
